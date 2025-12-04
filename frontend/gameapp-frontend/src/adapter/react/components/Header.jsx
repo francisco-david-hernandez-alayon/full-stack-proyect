fc79ba278650949d2019506f901fe1a083674293
david-hernandez <alu0101469898@ull.edu.es>
2025-12-04 10:26:52 +0000

    pequeños cambios frontend

--- frontend/gameapp-frontend/src/adapter/react/components/Header.jsx
+++ frontend/gameapp-frontend/src/adapter/react/components/Header.jsx
@@ -2,10 +2,27 @@ import React from 'react'
 
 export const Header = () => {
   return (
-    <div>
-            <h1 className="text-3xl font-bold underline">
-                HEADER
-            </h1>
+    <header className=" bg-rose-800">
+      <nav className="mx-auto flex items-center justify-between p-6">
+        <div className="flex lg:flex-1">
+          <a href="#" className="-m-1.5 p-1.5">
+            <span className="sr-only">Your Company</span>
+            <img src="https://images.vexels.com/media/users/3/326801/isolated/preview/a1c22a1b6fc6218aa769f0ae88dc2584-espada-de-dibujos-animados.png" alt="" className="h-8 w-auto" />
+          </a>
         </div>
+
+        <el-popover-group className="hidden lg:flex lg:gap-x-12">
+          <a href="#" className="text-sm/6 font-semibold text-white">Games</a>
+          <a href="#" className="text-sm/6 font-semibold text-white">Characters</a>
+          <a href="#" className="text-sm/6 font-semibold text-white">Biomes</a>
+          <a href="#" className="text-sm/6 font-semibold text-white">Enemys</a>
+          <a href="#" className="text-sm/6 font-semibold text-white">Items</a>
+        </el-popover-group>
+
+        <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+          <a href="#" className="text-sm/6 font-semibold text-white">How to play <span aria-hidden="true">&rarr;</span></a>
+        </div>
+      </nav>
+    </header>
   )
 }
